0e8a3d8d0f0a59dbc34c42007a3f4a915ce1500b
AlekseyK <aleksey.kabishau@gmail.com>
2019-01-30 01:26:50 -0500

    updates

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -3,17 +3,27 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
     @IBAction func recordAudio(_ sender: Any) {
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
     @IBAction func stopRecording(_ sender: Any) {
-        print("Stop recording")
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+        recordingLabel.text = "Tap to Record"
     }
 }
 
