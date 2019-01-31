031f08361b871ad842730073ccc07b10b6284b27
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-01-31 10:47:33 -0500

    cleaning

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -47,33 +47,3 @@ class RecordSoundsViewController: UIViewController {
         try! audioSession.setActive(false, options: [])
     }
 }
-
-/*
-import AVFoundation
-
-class ViewController: UIViewController {
-    
-    // MARK: Properties
-    
-    var audioRecorder: AVAudioRecorder!
-    
-    @IBAction func recordAudio(_ sender: AnyObject) {
-        recordingLabel.text = "Recording in progress"
-        stopRecordingButton.isEnabled = true
-        recordButton.isEnabled = false
-        
-        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
-        let recordingName = "recordedVoice.wav"
-        let pathArray = [dirPath, recordingName]
-        let filePath = URL(string: pathArray.joined(separator: "/"))
-        
-        let session = AVAudioSession.sharedInstance()
-        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
-        
-        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
-        audioRecorder.isMeteringEnabled = true
-        audioRecorder.prepareToRecord()
-        audioRecorder.record()
-    }
-}
-*/
