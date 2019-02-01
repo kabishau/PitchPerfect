fe05b79e425293e413ce2e0cb65753eb16191a3e
AlekseyK <aleksey.kabishau@gmail.com>
2019-02-01 00:41:46 -0500

    completing autolayout using stack view setting button's imageView to be aspectFit to fix layout for small devices in horizontal mode

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -2,10 +2,32 @@ import UIKit
 
 class PlaySoundsViewController: UIViewController {
     
-    var recordedAudioURL: URL!
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
 
+    var recordedAudioURL: URL!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        snailButton.imageView?.contentMode = .scaleAspectFit
+        chipmunkButton.imageView?.contentMode = .scaleAspectFit
+        rabbitButton.imageView?.contentMode = .scaleAspectFit
+        vaderButton.imageView?.contentMode = .scaleAspectFit
+        echoButton.imageView?.contentMode = .scaleAspectFit
+        reverbButton.imageView?.contentMode = .scaleAspectFit
     }
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("play pressed")
+    }
+    @IBAction func stopButtonPressed(_ sender: Any) {
+        print("Stop pressed")
+    }
+    
+    
 }
